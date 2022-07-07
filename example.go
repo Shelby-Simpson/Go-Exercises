23e4c77e219f4ea89cc0b231713d4949b20a2b70
Shelby-Simpson <shelbysimpson98@gmail.com>
2022-07-07 13:16:21 -0400

    Added map activity: keyword search

--- example.go
+++ example.go
@@ -1,22 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
-type acc struct {
-	name   string
-	number int
+func mapFromFn(item map[string]float64) map[string]float64 {
+	db := make(map[string]float64)
+	for k, v := range item {
+		db[k] = v * 2
+	}
+	return db
 }
 
 func main() {
-	temp := map[string]acc{"Me": {name: "Me", number: 1},
-		"You": {name: "You", number: 2}}
-	key := make([]string, 0, len(temp))
-	for i := range temp {
-		key = append(key, i)
-	}
-	sort.Strings(key)
-	fmt.Println(key)
+	p := make(map[string]float64)
+	p["iPhone"] = 99256.50
+	p["Android"] = 5000
+	fmt.Println(mapFromFn(p))
 }
+
+// func main() {
+// 	mapData := map[string]interface{}{
+// 		"Name":    "noknown",
+// 		"Age":     23,
+// 		"Admin":   true,
+// 		"Hobbies": []string{"IT", "Travel"},
+// 		"Complex": struct {
+// 			real int
+// 			img  int
+// 		}{12, -1},
+// 	}
+// 	fmt.Println(mapData)
+// }
