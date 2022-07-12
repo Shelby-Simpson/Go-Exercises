dff42167e8baeab2780c51b5100fe0f31eceafc2
Shelby-Simpson <shelbysimpson98@gmail.com>
2022-07-12 15:56:43 -0400

    Started Goblin Tower

---
+++ Module6ActivityGoblinTower.go
@@ -0,0 +1,21 @@
+// Shelby Simpson
+// Module 6 Activity: Goblin Tower
+package main
+
+type hero struct {
+	health  int
+	attack  int
+	defense int
+	potions []int
+	gold    int
+}
+
+type goblin struct {
+	health  int
+	attack  int
+	defense int
+}
+
+func main() {
+
+}
