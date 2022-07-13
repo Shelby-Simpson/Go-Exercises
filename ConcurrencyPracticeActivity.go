ab8b1957dda87ba6a3812437ee92409d3200b273
Shelby-Simpson <shelbysimpson98@gmail.com>
2022-07-13 12:54:06 -0400

    Executed concurrent functions in order

--- ConcurrencyPracticeActivity.go
+++ ConcurrencyPracticeActivity.go
@@ -10,14 +10,15 @@ import (
 var wg sync.WaitGroup
 
 func main() {
-	wg.Add(2)
+	wg.Add(1)
 	go func() {
 		for i := 100; i >= 0; i-- {
 			fmt.Println("Anon func 1:", i)
 		}
 		wg.Done()
 	}()
-
+	wg.Wait()
+	wg.Add(1)
 	go func() {
 		for i := 0; i <= 100; i++ {
 			fmt.Println("Anon func 2:", i)
